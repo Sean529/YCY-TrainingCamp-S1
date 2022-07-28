192148226f5b5cc20ccb9e79efe065e06bd5fd72
Sean <atxiesz@gmail.com>
2022-07-28 08:26:46 +0800

    feat: 加载资源并渲染在画布上

--- src/js/brakebanner.js
+++ src/js/brakebanner.js
@@ -1,5 +1,26 @@
 class BrakeBanner{
 	constructor(selector){
-		
+		// 初始化 pixi 应用，画布的宽高、颜色
+		this.app = new PIXI.Application({
+			width: window.innerWidth,
+			height: window.innerHeight,
+			backgroundColor: 0xff0000
+		})
+
+		// 把实例在画布渲染
+		document.querySelector(selector).appendChild(this.app.view)
+		// 创建加载器，加载（图片）资源
+		this.loader = new PIXI.Loader()
+		// 加载指定图片
+		this.loader.add('btn.png', 'images/btn.png')
+		// 加载资源
+		this.loader.load()
+		// 加载成功回调
+		this.loader.onComplete.add(() => {
+			// 精灵图实例
+			const btn = new PIXI.Sprite(this.loader.resources['btn.png'].texture)
+			// 将精灵图添加到画布
+			this.app.stage.addChild(btn)
+		})
 	}
 }
